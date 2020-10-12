9b32820b04a06a4538b1dd3dbcb6544615c9536a
Jochman <bhochman@paloaltonetworks.com>
2020-10-12 15:53:41 +0300

    gifs

--- 3.9/tell_me_why.py
+++ 3.9/tell_me_why.py
@@ -1,8 +1,8 @@
 from hints import backstreet_singer
 
 tell_me_why = "Tell me why"
-tell_me_why1 = {"Ain't nothin' but a heartache": tell_me_why}
-tell_me_why2 = {"Ain't nothin' but a mistake": tell_me_why}
-tell_me_why3 = {"I never wanna hear you say": tell_me_why}
+back = {"Ain't nothin' but a mistake": tell_me_why}
+street = {"I never wanna hear you say": tell_me_why}
+boys = {"Ain't nothin' but a heartache": tell_me_why}
 
 backstreet_singer("CHANGE THIS LINE ONLY")  # TODO: sing the chorus
