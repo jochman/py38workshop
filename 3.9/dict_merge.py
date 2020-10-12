853b8a6e68dd06698ae54ccbf432163dd6b93979
Jochman <bhochman@paloaltonetworks.com>
2020-10-12 16:10:02 +0300

    gifs

--- 3.9/dict_merge.py
+++ 3.9/dict_merge.py
@@ -3,6 +3,6 @@ from hints import backstreet_singer
 tell_me_why = {"But we are two worlds apart": "Can't reach to your heart"}
 aint_nothing_but_a_heartache = {"When you say": "That I want it that way"}
 
-second_verse = tell_me_why | aint_nothing_but_a_heartache
+second_verse = dict(**tell_me_why, **aint_nothing_but_a_heartache)
 
 backstreet_singer(second_verse)
--- 3.9/example.py
+++ 3.9/example.py
@@ -1,6 +1,4 @@
-from typing import Dict
-
-def what_is_love(dct: Dict[str, str]):
+def what_is_love(dct: dict[str, str]):
     for key, value in dct.items():
         print(key)
         print(value)
@@ -10,9 +8,13 @@ def what_is_love(dct: Dict[str, str]):
 love = {"What is love?": "Oh baby, don't hurt me"}
 shrek = {"Don't hurt me": "No more"}
 
-verse = dict(**love, **shrek)
-what_is_love(verse)
+verse = love | shrek
+#what_is_love(verse)
 
 
-love.update(shrek)
-what_is_love(love)
+love |= shrek
+#what_is_love(love)
+
+print("hod alpert".removeprefix("hod"))
+
+print("hod alpert".removesuffix("alpert"))
