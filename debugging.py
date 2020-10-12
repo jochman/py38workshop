7d45f6b6d113daccb90a15311279810f64341d54
Jochman <bhochman@paloaltonetworks.com>
2020-10-12 13:09:25 +0300

    -a
    tell me why

--- debugging.py
+++
@@ -1,5 +0,0 @@
-name = "Donald Trump"
-is_annoying = True
-
-print(f"name={name}")
-print(f"is_annoying={is_annoying}")
--- positional.py
+++
@@ -1,4 +0,0 @@
-def simple_calc(x, y, z):
-    return x + y * z
-
-print(simple_calc(5, z=2 ,y=10))
